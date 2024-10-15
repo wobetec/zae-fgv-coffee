affcf057187df5613744e709a24bbf422e597dcc
wobetec <esdras2821@gmail.com>
2024-10-15 20:31:13 -0300

    Create authentication view with rest framework

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -1,10 +1,5 @@
 """
 ASGI config for backend project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
 import os
