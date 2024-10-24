6785adad10e7a297c13161baafead21f83e95bd2
Zuilho Segundo <segundozuilho@gmail.com>
2024-10-23 23:55:16 -0300

    add models

--- backend/app/models.py
+++ backend/app/models.py
@@ -21,21 +21,65 @@ class VendingMachine(models.Model):
 
 
 class Stock(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='stocks')
+    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE, related_name='stocks')
     stock_quantity = models.IntegerField()
+    stock_date_update = models.DateTimeField(auto_now=True)
 
     class Meta:
         unique_together = ('product', 'vending_machine')
 
 
+class SupportUser(User):
+    pass
+
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True, auto_created=True)
+    order_total = models.DecimalField(max_digits=100, decimal_places=2)
+    order_date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE, related_name='orders')
+
+    def __str__(self):
+        return f'Order {self.order_id}'
+
+
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='ratings')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='ratings')
+    rating_star = models.IntegerField()
+    rating_description = models.CharField(max_length=140)
+    rating_date_update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = ('user', 'product', 'rating_date_update')
+
+    def __str__(self):
+        return f'Rating by {self.user.username} for {self.product.prod_name} on {self.rating_date_update}'
+
+
+class Sell(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='sells')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='sells')
+    sell_quantity = models.IntegerField()
+    sell_create_date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('order', 'product')
+
+    def __str__(self):
+        return f'Sell {self.product.prod_name} in Order {self.order.order_id}'
+
+
 class Wishlist(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='wishlist')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='wishlist')
+    vending_machine = models.ForeignKey(VendingMachine, on_delete=models.CASCADE, related_name='wishlist')
 
     class Meta:
         unique_together = ('user', 'product', 'vending_machine')
-    
+
     def __str__(self):
-        return self.product.prod_name + ' in ' + self.vending_machine.vm_id + ' for ' + self.user.username
+        return f'{self.product.prod_name} in {self.vending_machine.vm_id} for {self.user.username}'
+
