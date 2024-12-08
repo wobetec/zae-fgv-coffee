6c6b416081a2109262e08444a3d004b787805dd8
Esdras Cavalcanti de Almeida <39674011+wobetec@users.noreply.github.com>
2024-12-08 01:13:49 -0300

    new simple_report endpoint

---
+++ app/lib/api/endpoints/endpoint_simple_report.dart
@@ -0,0 +1,14 @@
+import 'package:http/http.dart';
+import 'endpoint.dart';
+
+class EndPointSimpleReport extends EndPoint {
+  Future<Response> getSimpleReport(String token, String date) async {
+    final url = Uri.parse('${EndPoint.baseUrl}report?date=$date');
+
+    final response = await get(
+      url,
+      headers: headers(token),
+    );
+    return response;
+  }
+}
